1c7dceff1c75b666d994b8283af78968365e3f47
Connor <kor.connor@gmail.com>
2023-03-01 21:30:25 +0900

    super add

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-func main() {
-	const name string = "nico"	
+import "fmt"
 
+func superAdd(numbers ...int) int {
+	total := 0
+
+	for index, number := range numbers {
+		fmt.Println(index, number)
+		total += number
+	}
+	return total
+}
+
+func main() {
+
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 
 }
