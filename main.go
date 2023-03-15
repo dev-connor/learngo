5b299825fee4448401e7732bf028fbbbae41986a
Connor <kor.connor@gmail.com>
2023-03-15 20:47:59 +0900

    고루틴

--- main.go
+++ main.go
@@ -2,24 +2,20 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/dev-connor/learngo/myDict"
+	"time"
 )
 
-type Dictionary map[string]string
-
 func main() {
-	dictionary := myDict.Dictionary{}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
-	if err != nil {
-		fmt.Println(err)
+	go sexyCount("nico")
+	go sexyCount("flynn")
+	time.Sleep(time.Second * 5)
+
+}
+
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
 	}
-	hello, err := dictionary.Search(word)
-	fmt.Println(hello)
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
+
 }
