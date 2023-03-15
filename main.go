cee6e863b2b3ed7a285994bb20ca14f84f6037a2
Connor <kor.connor@gmail.com>
2023-03-15 20:59:46 +0900

    고루틴과 채널

--- main.go
+++ main.go
@@ -6,16 +6,18 @@ import (
 )
 
 func main() {
-	go sexyCount("nico")
-	go sexyCount("flynn")
-	time.Sleep(time.Second * 5)
+	c := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, c)
 
+	}
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	// time.Sleep(time.Second * 10)
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
-
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	c <- true
 }
